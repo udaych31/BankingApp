3510c86453e64e9852f9491dd10858b1fa56e9e8
admin1104 <49300434+admin1104@users.noreply.github.com>
2019-05-30 16:00:53 +0530

    changes

--- src/main/java/com/hcl/bank/app/controller/UserController.java
+++ src/main/java/com/hcl/bank/app/controller/UserController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hcl.bank.app.dto.LoginRequest;
